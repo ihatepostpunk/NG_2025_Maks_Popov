cb8a9c962cf288e20f7e421d4cbf7aba5726d29e
Maks_Popov <deffet.gon@gmail.com>
2025-11-08 13:37:38 +0200

    The function has been removed

--- Lesson_1/Task_2/main.cpp
+++ Lesson_1/Task_2/main.cpp
@@ -1,57 +1,63 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 
-void showResult(float result) {
-    cout << "Result: " << result << endl;
-}
+using namespace std;
 
 int main() {
     float userNumber1 = 0;
     float userNumber2 = 0;
     char operation;
+    float result = 0;
+    bool valid = true;
 
     cout << "Enter the first number: ";
     cin >> userNumber1;
 
-    cout << "Enter the operation:\n"
-            "1) +\n"
-            "2) -\n"
-            "3) *\n"
-            "4) /\n"
-            "5) Degree\n"
-            "6) Root\n";
+    cout << "Enter the operation:" << endl
+         << "1) +" << endl
+         << "2) -" << endl
+         << "3) *" << endl
+         << "4) /" << endl
+         << "5) Degree" << endl
+         << "6) Root" << endl;
+
     cin >> operation;
 
     if (operation == '6') {
-        showResult(sqrt(userNumber1));
+        result = sqrt(userNumber1);
     } else {
         cout << "Enter the second number: ";
         cin >> userNumber2;
 
         switch (operation) {
         case '1':
-            showResult(userNumber1 + userNumber2);
+            result = userNumber1 + userNumber2;
             break;
         case '2':
-            showResult(userNumber1 - userNumber2);
+            result = userNumber1 - userNumber2;
             break;
         case '3':
-            showResult(userNumber1 * userNumber2);
+            result = userNumber1 * userNumber2;
             break;
         case '4':
-            if (userNumber2 == 0)
+            if (userNumber2 == 0) {
                 cout << "Division by zero" << endl;
-            else
-                showResult(userNumber1 / userNumber2);
+                valid = false;
+            } else {
+                result = userNumber1 / userNumber2;
+            }
             break;
         case '5':
-            showResult(pow(userNumber1, userNumber2));
+            result = pow(userNumber1, userNumber2);
             break;
         default:
             cout << "Unknown command" << endl;
+            valid = false;
         }
     }
+
+    if (valid)
+        cout << "Result: " << result << endl;
 
     return 0;
 }
