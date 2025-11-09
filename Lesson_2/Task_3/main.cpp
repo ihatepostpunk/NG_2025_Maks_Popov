3fa6e75f6b7de8dc85fe5e257ba447332ab50370
Maks_Popov <deffet.gon@gmail.com>
2025-11-09 15:59:28 +0200

    Change language

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
-#include <locale>
+#include <cctype>
 
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "");
-
-    const wchar_t text[] = L"Вот у меня появился вопрос, ответ на который меня очень сильно тревожит: а можете ли вы вывести мне на экран соотношение гласных букв  к негласным? Сколько процентов в тексте занимают гласные символы, а сколько негласные?";
+    const char text[] = "Once upon a time, a curious cat wandered into a mysterious garden.";
 
     int vowelsLetter = 0;
     int nonvowelsLetter = 0;
 
-    for (int index = 0; text[index] != L'\0'; index++) {
-        wchar_t symbol = text[index];
+    for (int index = 0; text[index] != '\0'; index++) {
+        char symbol = text[index];
 
-        if (symbol >= L'А' && symbol <= L'Я') symbol += L'а' - L'А';
+        if (isalpha(symbol)) {
+            char lower = tolower(symbol);
 
-        if (symbol >= L'а' && symbol <= L'я') {
-            if (symbol == L'а' || symbol == L'е' || symbol == L'ё' || symbol == L'и' ||
-                symbol == L'о' || symbol == L'у' || symbol == L'ы' || symbol == L'э' ||
-                symbol == L'ю' || symbol == L'я') {
+            if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
                 vowelsLetter++;
             } else {
                 nonvowelsLetter++;
@@ -28,8 +24,8 @@ int main() {
     }
 
     int total = vowelsLetter + nonvowelsLetter;
-    wcout << L"Vowels: " << (double)vowelsLetter / total * 100 << L"%" << endl;
-    wcout << L"Non-vowels: " << (double)nonvowelsLetter / total * 100 << L"%" << endl;
+    cout << "Vowels: " << (double)vowelsLetter / total * 100 << "%" << endl;
+    cout << "Consonants: " << (double)nonvowelsLetter / total * 100 << "%" << endl;
 
     return 0;
 }
